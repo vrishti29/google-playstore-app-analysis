f6fef1de777fadc2c839af5e420829a4aaf78824
Vrishti Garg <105159048+vrishti29@users.noreply.github.com>
2024-08-01 15:28:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.set_page_config(page_title = 'Interactive Data analysis')
+st.title('Interactive EDA of Apps on Google Play Store')
 
-st.write('Hello world!')
+with st.expander('About the app'):
+  st.markdown("**What can thsi app do?**")
+  st.info("")
+  st.markdown('**How can you use the app?**')
+  st.warning('To engage with the app.')
+
+#Question header
+st.header('Which Genre apps are performing best on Google Play Store?')
+
+#Load data
+df = pd.read_csv('https://raw.githubusercontent.com/vrishti29/google-playstore-app-analysis/master/data/googleplaystore.csv')
