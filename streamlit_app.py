76b01dc0ead930fe75bd2493de4d636b7f95ebde
Vrishti Garg <105159048+vrishti29@users.noreply.github.com>
2024-08-05 10:49:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,20 @@ with st.expander('Data visualisation'):
 #Input Features in sidebar
 with st.sidebar:
   st.header('Input Features')
-  category = st.selectbox('Category', ('ART_AND_DESIGN', 'BEAUTY', 'BOOKS_AND_REFERENCE', 'BUSINESS',
-       'COMICS', 'DATING', 'EDUCATION', 'EVENTS', 'FOOD_AND_DRINK',
-       'HOUSE_AND_HOME', 'LIBRARIES_AND_DEMO', 'FAMILY', 'MEDICAL',
-       'TOOLS', 'PARENTING', 'VIDEO_PLAYERS', 'PERSONALIZATION', 'GAME',
-       'PHOTOGRAPHY', 'SOCIAL', 'SPORTS', 'COMMUNICATION', 'PRODUCTIVITY',
-       'AUTO_AND_VEHICLES', 'FINANCE', 'LIFESTYLE', 'TRAVEL_AND_LOCAL',
-       'SHOPPING', 'HEALTH_AND_FITNESS', 'WEATHER', 'NEWS_AND_MAGAZINES',
-       'MAPS_AND_NAVIGATION', 'ENTERTAINMENT'))
-  with st.echo():
-    st.write("This code will be printed to the sidebar.")
+  category = st.selectbox('Category', ('AUTO_AND_VEHICLES', 'ART_AND_DESIGN', 'BEAUTY', 'BOOKS_AND_REFERENCE', 'BUSINESS',
+       'COMICS','COMMUNICATION', 'DATING', 'EDUCATION', 'ENTERTAINMENT', 'EVENTS', 'FOOD_AND_DRINK', 'FAMILY', 'FINANCE',
+       'GAME', 'HOUSE_AND_HOME', 'HEALTH_AND_FITNESS', 'LIBRARIES_AND_DEMO','LIFESTYLE', 'MEDICAL','NEWS_AND_MAGAZINES',
+       'TOOLS', 'PARENTING', 'VIDEO_PLAYERS', 'PERSONALIZATION','PHOTOGRAPHY', 'SOCIAL', 'SPORTS', 
+       'PRODUCTIVITY',  'TRAVEL_AND_LOCAL', 'SHOPPING','WEATHER', 'MAPS_AND_NAVIGATION'))
+  rating = st.slider('Rating', 0,1., 1.2, 1.9, 5. , 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4. ,
+       3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3. , 2.9, 2.8, 2.7,
+       2.6, 2.5, 2.4, 2.3, 2.2, 2.1, 2. , 1.8, 1.7, 1.6, 1.5, 1.4,)
 
-  with st.spinner("Loading..."):
-    time.sleep(5)
-  st.success("Done!")
+  type = st.electbox('Type', ('free', 'paid'))
+
+  data = {'category' : category,
+          'rating' : rating,
+          'type' : type}
+
+  input_df = pd.DataFrame(data, index[0])
+  
